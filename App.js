01c9685b4717bff5824996fe9c02ee3eca2d8a0f
Htoo Aung Lin <59634121+Htoo23@users.noreply.github.com>
2022-10-20 10:31:14 +0630

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import Navbar from "./Components/Navbar/Navbar";
+import './App.css'
+import Intro from "./Components/Intro/Intro";
+import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
+import Works from "./Components/Works/Works";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonial from "./Components/Testimonials/Testimonial";
+import Contact from "./Components/Contact/Contact";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Intro/>
+      <Services/>
+      <Experience/>
+      <Works/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
